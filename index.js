bccbac42ef4837175bb25184ab9f65c501fdcd12
Anthony Chang <anthony.chang@utexas.edu>
2019-09-21 16:42:34 -0500

    Retrieve data from xlsx sheet in json format

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const logger = require('koa-logger');
+const XLSX = require('xlsx');
 
 app = new Koa();
 router = new Router();
+app.use(logger());
 
-router.get("/test", (ctx) => {
+/*router.get("/test", (ctx) => {
   ctx.body = "test";
+});*/
+const workbook = XLSX.readFile("RiceHackathonFile.xlsx");
+
+router.get("/get-worker-details", (ctx) => {
+  ctx.body = XLSX.utils.sheet_to_json(workbook["Sheets"]["Worker Details"]);
+});
+
+router.get("/get-equipment-details", (ctx) => {
+  ctx.body = XLSX.utils.sheet_to_json(workbook["Sheets"]["Equipment Details"]);
+});
+
+router.get("/get-facility-details", (ctx) => {
+  ctx.body = XLSX.utils.sheet_to_json(workbook["Sheets"]["Facility Details"]);
 });
 
 app.use(router.routes());
