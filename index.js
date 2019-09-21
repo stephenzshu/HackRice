1415141745a75c7fe7c6c5571e6297e2e9ea0806
Anthony Chang <anthony.chang@utexas.edu>
2019-09-21 17:09:45 -0500

    endpoints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
 const XLSX = require('xlsx');
+const spawn = require('child_process');
 
 app = new Koa();
 router = new Router();
@@ -22,6 +23,14 @@ router.get("/get-equipment-details", (ctx) => {
 
 router.get("/get-facility-details", (ctx) => {
   ctx.body = XLSX.utils.sheet_to_json(workbook["Sheets"]["Facility Details"]);
+});
+
+router.get("/get-current-work-order", (ctx) => {
+  // arg1 will be function to call, arg2 ... will be parameters for function call
+  const pythonProcess = spawn.spawn('python', ["./main.py", arg1, arg2]);
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(data);
+  });
 });
 
 app.use(router.routes());
