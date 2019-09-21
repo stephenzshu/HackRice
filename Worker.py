3c4df6a1a4c5c9001136bafe2e2632472f7fc4ba
Anthony Chang <anthony.chang@utexas.edu>
2019-09-21 17:44:24 -0500

    worker class

---
+++ Worker.py
@@ -0,0 +1,18 @@
+class Worker:
+    def __init__(self, name, equipment_cert, shifts, current_task):
+        self.name = name
+        self.equipment_cert = equipment_cert
+        self.shifts = shifts
+        self.current_task = current_task
+
+    def get_name(self):
+        return self.name
+
+    def get_equipment_cert(self):
+        return self.equipment_cert
+
+    def get_shifts(self):
+        return self.shifts
+
+    def get_current_task(self):
+        return self.current_task
