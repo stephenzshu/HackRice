8f50fec51818d3a9c6cf91c2786785cf81e0669d
ericwen22 <49817277+ericwen22@users.noreply.github.com>
2019-09-22 08:57:49 -0500

    Add files via upload

---
+++ Cheveron 4/Cheveron 4Tests/Cheveron_4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Cheveron_4Tests.swift
+//  Cheveron 4Tests
+//
+//  Created by zhiying wen on 9/22/19.
+//  Copyright © 2019 Hack Rice. All rights reserved.
+//
+
+import XCTest
+@testable import Cheveron_4
+
+class Cheveron_4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
