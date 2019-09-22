469e2bf008b378c1fe7553cf68b296571c1b67e9
Anthony Chang <anthony.chang@utexas.edu>
2019-09-22 00:35:57 -0500

    post requests

--- router/router.js
+++ router/router.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
 const XLSX = require('xlsx');
 const spawn = require('child_process');
 const request = require('superagent');
@@ -8,6 +9,7 @@ const request = require('superagent');
 app = new Koa();
 router = new Router();
 app.use(logger());
+app.use(bodyParser());
 
 /*router.get("/test", (ctx) => {
   ctx.body = "test";
@@ -26,18 +28,20 @@ router.get("/get-facility-details", (ctx) => {
   ctx.body = XLSX.utils.sheet_to_json(workbook["Sheets"]["Facility Details"]);
 });
 
-// Test
-router.get("/meta-test", (ctx) => {
-  request.get("https://morning-headland-65470.herokuapp.com/get-worker-details")
-    .set('Accept', 'application/json')
-    .then(res => {
-      console.log(res);
-    });
+router.get("/post-test", (ctx) => {
+  request.post("https://morning-headland-65470.herokuapp.com/get-new-work-order")
+    .send('test')
+    .catch(err => {
+      console.log(err);
+    })
 });
 
-router.post("/get-new-work-order", workerName, (ctx) => {
+router.post("/get-new-work-order", bodyParser, (ctx) => {
   // arg1 will be function to call, arg2 ... will be parameters for function call
-  let json;
+  console.log("yes");
+  let test = ctx.request.body;
+  ctx.body = test;
+  /*let json;
   let arg1 = workerName;
   const pythonProcess = spawn.spawn('python', ["./main.py", "retrieveWork", arg1]);
   pythonProcess.stdout.on('data', (data) => {
@@ -52,7 +56,7 @@ router.post("/get-new-work-order", workerName, (ctx) => {
       "inProgress": null
     }
   });
-  ctx.body = json;
+  ctx.body = json;*/
 });
 
 router.get("/finish-current-work-order", (ctx) => {
@@ -62,6 +66,15 @@ router.get("/finish-current-work-order", (ctx) => {
     ctx.status = 200;
     ctx.body = "Current work order successfully removed";
   });
+});
+
+// Test routes
+router.get("/get-test", (ctx) => {
+  request.get("https://morning-headland-65470.herokuapp.com/get-worker-details")
+    .set('Accept', 'application/json')
+    .then(res => {
+      console.log(res);
+    });
 });
 
 app.use(router.routes());
